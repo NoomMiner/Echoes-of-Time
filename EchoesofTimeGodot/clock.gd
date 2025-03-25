d932eee49ec020dfd49a579d76cb3d68e39bd78d
Skyler Guard <skylerforestguard@gmail.com>
2025-03-24 19:16:58 -0700

    Working collision w/ clock

--- EchoesofTimeGodot/clock.gd
+++ EchoesofTimeGodot/clock.gd
@@ -1,14 +1,12 @@
 extends Node2D
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 	
-func _on_area_entered(body):
-	print("HELLO")
-	visible = false
+#func bodyDetection(_body):
+	#print("HELLO")
+	#visible = false
