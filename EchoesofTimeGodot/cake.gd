09eaea68990b7307d87098eea87ff13967a4f1c4
Skyler Guard <skylerforestguard@gmail.com>
2025-03-25 17:44:15 -0700

    Added cake scene (separate for now)

---
+++ EchoesofTimeGodot/cake.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+var assoText = RichTextLabel
+var isClose = false
+var isEaten = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	assoText = get_child(1)
+	set_process_input(true) 
+
+func _input(ev):
+	if Input.is_key_pressed(KEY_Q):
+		if (isClose == true):
+			if (not isEaten):
+				assoText.visible = false
+				assoText = get_child(2)
+				isEaten = true
+				get_child(3).visible = true
+				## change sprite?
+			else:
+				assoText.visible = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if (not isEaten):
+		assoText.visible = true
+	isClose = true
+
+func _on_area_2d_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	assoText.visible = false
+	isClose = false
