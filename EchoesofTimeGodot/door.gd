a47346fb64532a7590f27f2f2adc0e7c62276c6c
Tyler <tyler.bryant117@gmail.com>
2025-04-13 21:32:07 -0700

    Fixed cake scene camera

--- EchoesofTimeGodot/door.gd
+++ EchoesofTimeGodot/door.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	print(isClose)
 	pass
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
