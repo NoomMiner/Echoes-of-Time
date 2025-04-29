7de27be8b91d58d371a950b9457db2ed86d7be3d
Skyler Guard <139205457+SkylerGuard@users.noreply.github.com>
2025-04-28 18:44:03 -0700

    added pseudocode for cake unit testing

---
+++ EchoesofTimeGodot/test/unit/test_cake.gd
@@ -0,0 +1,19 @@
+extends GutTest
+
+# in cake scene, away from cake
+  # assert:
+    # no msg
+    # cake sprite uneaten
+
+# in cake scene, next to cake
+  # assert:
+    # prompt is present
+
+# press 'q' next to cake
+  # assert:
+    # eaten cake sprite
+    # prompt changes
+
+# press 'q' again
+  # assert:
+    # flavour text changes
