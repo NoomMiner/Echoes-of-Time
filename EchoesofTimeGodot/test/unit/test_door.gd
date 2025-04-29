41ef87b99402be0950cc74c0a7d1ded0024cea84
Skyler Guard <139205457+SkylerGuard@users.noreply.github.com>
2025-04-28 20:16:07 -0700

    implement test_door.gd

--- EchoesofTimeGodot/test/unit/test_door.gd
+++ EchoesofTimeGodot/test/unit/test_door.gd
@@ -1,24 +1,71 @@
 extends GutTest
 
-# game state = 0, repeat for game state = 1
+var level
+var player
 
-  # with player away from door
-  # assert
-    # rich text not visible
-    # pressing 'q' does nothing
+func before_each():
+    level = preload("res://node_2d.tscn").instantiate()
+    add_child(level)
 
-  # with player next to door
-  # assert
-    # rich text: locked door
-    # pressing 'q' does nothing
+    # create fake 'change scene' function
+    gut.double(level.get_tree(), "change_scene_to_file")
 
-# game state = 2
-  # with player away from door
-  # assert
-    # rich text not visible
-    # pressing 'q' does nothing
+    player = level.get_node("CharacterBody2D")
 
-  # with player next to door
-  # assert
-    # rich text: use key?
-    # pressing 'q' moves to next room
+func after_each():
+    level.queue_free()
+
+func simulate_q_press(target):
+    var ev = InputEventKey.new()
+    ev.keycode = KEY_Q
+    ev.pressed = true
+    target._input(ev)
+
+func test_no_key():
+    assert_false(level.get_node("Door/usekeyprompt").visible)
+    assert_false(level.get_node("Door/doorlocked").visible)
+
+    player.global_position = level.get_node("Door").global_position
+
+    assert_true(level.get_node("Door/doorlocked").visible)
+    assert_false(level.get_node("Door/usekeyprompt").visible)
+
+    simulate_q_press(level)
+
+    assert_not_called(level.get_tree(), "change_scene_to_file", "Attempts to change scene without key")
+
+
+@test
+func test_state_0():
+    level.state = 0
+    test_no_key()
+
+@test
+func test_state_1():
+    level.state = 1
+    test_no_key()
+
+@test
+func test_state_2_player_away():
+    level.state = 2
+
+    assert_false(level.get_node("Door/doorlocked").visible)
+    assert_false(level.get_node("Door/usekeyprompt").visible)
+
+    simulate_q_press(level)
+
+    assert_not_called(level.get_tree(), "change_scene_to_file", "Attempts to change scene while away")
+
+@test
+func test_state_2_player_near():
+    level.state = 2
+    player.global_position = level.get_node("Door").global_position
+
+    assert_false(level.get_node("Door/doorlocked").visible)
+    assert_true(level.get_node("Door/usekeyprompt").visible)
+
+    assert_not_called(level.get_tree(), "change_scene_to_file", "Attempts to change scene before pressing 'q'")
+
+    simulate_q_press(level)
+
+    assert_called(level.get_tree(), "change_scene_to_file", "Does not attempt to change scene")
